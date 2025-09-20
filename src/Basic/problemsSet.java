b003378d25ba02f3ac6fde03bbd059c51c8d813a
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-20 15:36:01 +0530

    Initial commit for Java Learning

---
+++ src/Basic/problemsSet.java
@@ -0,0 +1,43 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class problemsSet {
+    public static void main(String[] args) {
+
+//        QUESTION 1
+//        int a = 43;
+//        int b = 32;
+//        int c = 423;
+//        int sum = a+b+c;
+//        System.out.println(sum);
+
+//        QUESTION 2
+//        float subject1 = 45;
+//        float subject2 = 55;
+//        float subject3 = 65;
+//        float cgpa = (subject1 + subject2 + subject3)/30;
+//        System.out.println(cgpa);
+
+
+//        QUESTION 3
+//        System.out.println("What is your name?");
+//        Scanner sc = new Scanner(System.in);
+//        String name =  sc.next();
+//        System.out.println("Hello " + name + " have a hood day!");
+
+//        QUESTION 4
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter distance in kilometers : ");
+//        double kilometers = sc.nextDouble();
+//        double miles = kilometers * 0.621371;
+//        System.out.println(kilometers + " kilometers = " + miles + " miles");
+
+//        QUESTION 5
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        System.out.println(sc.hasNextInt());
+
+
+    }
+}
