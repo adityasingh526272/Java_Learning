ac184ddf2cd45a0786d7eb171e56b54471542dbf
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-21 14:51:49 +0530

    Initial commit for Java Learning

---
+++ src/Basic/Precedence.java
@@ -0,0 +1,25 @@
+package Basic;
+
+public class Precedence {
+    public static void main(String[] args) {
+//        int a = 6*5-34/2;
+//        int b = 30/5-34*2;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        Quick Quiz
+//        int x = 5;
+//        int y = 6;
+//        int k = x * y/2;
+
+        int a = 4;
+        int b = 2;
+        int c = 6;
+
+        int k = b*b - (4*a*c)/(2*c);
+        int t = b*b - 4*a*c/2*c;
+        System.out.println(t);
+        System.out.println(k);
+
+    }
+}
