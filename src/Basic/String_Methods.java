ed3cf800ba4caaf3c20f100dff91b040ee7b6bee
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-21 17:09:12 +0530

    Initial commit for Java Learning

---
+++ src/Basic/String_Methods.java
@@ -0,0 +1,52 @@
+package Basic;
+
+import java.sql.SQLOutput;
+
+public class String_Methods {
+    public static void main(String[] args) {
+        String name = "Aadi";
+//        System.out.println(name);
+//        int value = name.length();
+//        System.out.println(value);
+
+//        String lstring = name.toLowerCase();
+//        System.out.println(lstring);
+
+//        String ustring = name.toUpperCase();
+//        System.out.println(ustring);
+
+//        String nonTrimmedString = "  Aadi  ";
+//        System.out.println(nonTrimmedString);
+
+
+//        String trimmedString = nonTrimmedString.trim();
+//        System.out.println(trimmedString);
+
+//        System.out.println(name.substring(1));
+
+//        System.out.println(name.substring(1,4));
+
+//        System.out.println(name.replace('i','t'));
+//        System.out.println(name.replace("Aadi","Thakur"));
+
+//        System.out.println(name.startsWith("I"));
+
+//        System.out.println(name.endsWith("I"));
+
+//        System.out.println(name.charAt(3));
+
+//        System.out.println(name.indexOf("ad"));
+
+//        System.out.println(name.indexOf("a",0));
+
+//        System.out.println(name.lastIndexOf("i"));
+
+//        System.out.println(name.lastIndexOf("d",3));
+
+//        System.out.println(name.equals("Aadi"));
+
+//        System.out.println(name.equalsIgnoreCase("aADi"));
+
+        System.out.println("I am escape sequence \n double quote");
+    }
+}
