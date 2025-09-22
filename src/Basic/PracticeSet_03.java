1f5dfee7992028105e16b5c94c59d2e5e4470b43
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-22 15:11:57 +0530

    Initial commit for Java Learning

---
+++ src/Basic/PracticeSet_03.java
@@ -0,0 +1,30 @@
+package Basic;
+
+public class PracticeSet_03 {
+    public static void main(String[] args) {
+
+//        Question 1
+//        String name = "Aditya Singh";
+//        name = name.toLowerCase();
+//        System.out.println(name);
+
+//        Question 2
+//        String text = "Aditya Singh";
+//        text = text.replace(" ", "_");
+//        System.out.println(text);
+
+//        Question 3
+//        String letter = "Dear <|name|>, Thanks a lot!";
+//        letter = letter.replace("<|name|>", "Aadi");
+//        System.out.println(letter);
+
+//        Question 4
+//        String myString = "This string contains   double and triple spaces";
+//        System.out.println(myString.indexOf("  "));
+//        System.out.println(myString.indexOf("    "));
+
+//        Question 5
+        String letter2 = "Dear Harry,\n\t This java courses is nice.\nThanks";
+        System.out.println(letter2);
+    }
+}
