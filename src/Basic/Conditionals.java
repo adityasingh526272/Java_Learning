621e88c2c34c0b7841dd8de63b9f8309f9192229
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-22 15:24:34 +0530

    Initial commit for Java Learning

---
+++ src/Basic/Conditionals.java
@@ -0,0 +1,13 @@
+package Basic;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int age = 8;
+        if (age>18){
+            System.out.println("You can drive");
+        }
+        else {
+            System.out.println("Not eligible");
+        }
+    }
+}
