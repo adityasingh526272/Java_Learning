da05c437594668111b2b416e10ea5a7bcf4fc754
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-23 18:34:04 +0530

    Initial commit for Java Learning

---
+++ src/Basic/Loops.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=4){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("End of the program.");
+//        while (true){
+//            System.out.println("I am an infinite while loop!");
+//        }
+    }
+}
