83a1e8cdc733868e0c8b6424d01eaaf5b21b5532
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-24 19:43:11 +0530

    Initial commit for Java Learning

---
+++ src/Basic/For_Loop.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class For_Loop {
+    public static void main(String[] args) {
+//        int i;
+//        for (i=1; i<=10; i++){
+//            System.out.println(i);
+//        }
+
+//        int n = 5;
+//        for (int i=0; i<n; i++){
+//            System.out.println(2*i+1);
+//        }
+
+        for (int i=5; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
