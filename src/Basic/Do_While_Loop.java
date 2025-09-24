af7ee62251070310b3a91787ec14fe0227f43c04
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-24 19:22:20 +0530

    Initial commit for Java Learning

---
+++ src/Basic/Do_While_Loop.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class Do_While_Loop {
+    public static void main(String[] args) {
+//        int b = 0;
+//        do {
+//            System.out.println(b);
+//            b++;
+//        }while (b<5);
+        
+        int a = 1;
+        int N = 1000;
+        do {
+            System.out.println(a);
+            a++;
+        }while (a<=N);
+    }
+}
