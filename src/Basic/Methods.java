b9fe6216f6f21d16a4da7daccea749cd2f7fcc06
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-26 00:47:26 +0530

    Initial commit for Java Learning

---
+++ src/Basic/Methods.java
@@ -0,0 +1,31 @@
+package Basic;
+
+public class Methods {
+    static int logic(int x, int y){
+        int z;
+        if (x>y){
+            z = x+y;
+        }
+        else {
+            z = (x+y) * 5;
+        }
+        return z;
+
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+//        Method invocation using object creation
+//        Methods obj = new Methods();
+//        c = obj.logic(a,b);
+        c = logic(a,b);
+        System.out.println(a+ " " +b);
+        int a1 = 4;
+        int b1 = 3;
+        int c1;
+        c1 = logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
