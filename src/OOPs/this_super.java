409c90b208260a6c30c70d590027d6136803be37
adityasingh526272 <adityasinghy526@gmail.com>
2025-09-29 14:36:16 +0530

    Initial commit for Java Learning

--- src/OOPs/this_super.java
+++ src/OOPs/this_super.java
@@ -1,4 +1,29 @@
 package OOPs;
 
+class EkClass{
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    EkClass(int a){
+        this.a = a;
+    }
+    public int returnone(){
+        return 1;
+    }
+}
+class DoClass extends EkClass{
+    DoClass(int c){
+        super(c);
+        System.out.println("Mai ek constructor hoon");
+    }
+}
 public class this_super {
+    public static void main(String[] args) {
+        EkClass e = new EkClass(5);
+        DoClass d = new DoClass(55);
+        System.out.println(e.getA());
+    }
 }
