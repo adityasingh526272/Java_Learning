46cce2b6ce4ccba129dcaa712f3cc1e51937ddc1
adityasingh526272 <adityasinghy526@gmail.com>
2025-10-02 16:30:08 +0530

    Initial commit for Java Learning

--- src/OOPs/Practice_Set_13.java
+++ src/OOPs/Practice_Set_13.java
@@ -9,14 +9,14 @@ class Practice13 extends Thread{
 }
 class Practice13b extends Thread{
     public void run() {
-        while (true){
-            try {
-                Thread.sleep(200);
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-            System.out.println("Welcome");
-        }
+//        while (false){
+//            try {
+//                Thread.sleep(200);
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+//            System.out.println("Welcome");
+//        }
     }
 }
 
@@ -24,7 +24,14 @@ public class Practice_Set_13 {
     public static void main(String[] args) {
         Practice13 p1 = new Practice13();
         Practice13b p2 = new Practice13b();
-        p1.start();
+        p1.setPriority(6);
+        p2.setPriority(9);
+        System.out.println(p1.getPriority());
+        System.out.println(p2.getPriority());
+        System.out.println(p1.getState());
+//        p1.start();
         p2.start();
+        System.out.println(p2.getState());
+        System.out.println(Thread.currentThread().getState());
     }
 }
